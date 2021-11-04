b4399c6f4e93b306a4a8811204194dbd13f9d146
Hathor Hamilton <koshknot@gmail.com>
2021-11-04 12:30:06 -0400

    commit tree d72a35c433054f933e20a6870087560d4617dbef

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+      items = user.items
+      else
+        return render json: { error: "User not found" }, status: :not_found
+      end  
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find_by(id: params[:id])
+    user = User.find_by(id: params[:user_id])
+  if item.user_id == user.id
+    render json: item, include: :user
+        else
+          render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  def create
+    item = Item.create(item_params)
+    user = User.find_by(id: params[:user_id])
+    if user
+      render json: item, include: :user, status: :created
+    else 
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
+  private 
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
